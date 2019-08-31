b8bbf9ca6f7d6922a7230a57336745792593906b
sbx_user1051 <sbx_user1051@169.254.57.221>
2019-08-31 17:15:29 +0000

    Sat Aug 31 17:15:29 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,6 +3,13 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 PRICES = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+PROMO = {'A': (3, 130), 'B': (2, 45)}
+
+
+
+def calculate_product_price(p_name, p_price):
+
+
 
 def checkout(skus):
     skus = skus.encode('utf-8')
@@ -10,5 +17,6 @@ def checkout(skus):
     basket_value = 0
     for product in basket.items():
         single_price = PRICES[product[0]]
-        
+
+
 
