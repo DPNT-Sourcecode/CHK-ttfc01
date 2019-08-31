b6da7972251ce7631c03cf1b6036d908b03f890b
sbx_user1051 <sbx_user1051@169.254.57.221>
2019-08-31 17:21:29 +0000

    Sat Aug 31 17:21:29 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,7 +8,7 @@ PROMO = {'A': (3, 130), 'B': (2, 45)}
 
 
 def calculate_product_price(p_name, p_quantity):
-    if p_name in PROMO:
+    if p_name in PROMO and p_quantity >= PROMO[p_name][0]:
         p_promo = PROMO[p_name]
         quant, price = p_promo
 
@@ -21,6 +21,7 @@ def checkout(skus):
     basket_value = 0
     for product in basket.items():
         single_price = PRICES[product[0]]
+
 
 
 
