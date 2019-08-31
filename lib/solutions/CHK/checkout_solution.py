e291ac73eff00db32f1b09c406cdcaf616e11b26
sbx_user1051 <sbx_user1051@169.254.57.221>
2019-08-31 17:06:29 +0000

    Sat Aug 31 17:06:29 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,4 +7,8 @@ PRICES = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
 def checkout(skus):
     skus = skus.encode('utf-8')
     basket = dict((product, skus.count(product)) for product in list(skus))
+    basket_value = 0
+    for product in basket.items():
+        single_price = PRICES[product[0]]
+        
 
