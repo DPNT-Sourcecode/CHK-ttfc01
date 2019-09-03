d24d3b6c1e15f8999d086f91fe206be6d7f117de
sbx_user1051 <sbx_user1051@169.254.219.181>
2019-09-03 19:36:50 +0000

    Tue Sep 03 19:36:50 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,8 +5,8 @@ from .basket import Basket
 
 PRICES = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10, 'G': 20, 'H': 10,
           'I': 35, 'J': 60, 'K': 80, 'L': 90, 'M': 15, 'N': 40, 'O': 10, 'P': 50,
-          'Q': 30, 'R': 50, 'S': 30, 'T': 20, 'U': 40, 'V': 50, 'W': 20, 'X': 90,
-          'Y': 10, 'Z': 50}
+          'Q': 30, 'R': 50, 'S': 20, 'T': 20, 'U': 40, 'V': 50, 'W': 20, 'X': 17,
+          'Y': 20, 'Z': 21}
 PROMO_MULTIPLE_ITEMS_CHEAPER = {'A': [(5, 200), (3, 130)],
                                 'B': [(2, 45)],
                                 'H': [(10, 80), (5, 45)],
@@ -32,3 +32,4 @@ def checkout(skus):
     basket_value = basket.get_basket_total_value()
 
     return basket_value
+
